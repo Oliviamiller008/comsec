26fb9d0858072b9b44216d999d6260c145f13e14
Oliviamiller008 <66766436+Oliviamiller008@users.noreply.github.com>
2022-02-22 16:00:10 -0600

    update

--- olivia.js
+++ olivia.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
 const port = 3000
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/miller', (req, res) => {
+  return res.send('Hello, Olivia!');
+});
 
-app.listen(port, () => {
+https
+  .createServer(
+    {
+      // ...
+      cert: fs.readFileSync('/etc/apache2/ssl/bloomcounty_eng_mu_edu_cert.pem'),
+      key: fs.readFileSync('/etc/apache2/ssl/privateKey.pem'),
+      // ...
+    },
+    app
+  )
+  .listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
